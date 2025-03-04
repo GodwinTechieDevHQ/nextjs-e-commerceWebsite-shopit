1c70c8e00ab05c3f707de034e1ff7f69d832e8df
Godwin Adakonye John <gj09042003@gmail.com>
2025-03-05 00:51:34 +0100

    Fixed Redux issue in ProfileBtn component

--- src/components/layout/Navbar/TopNavbar/ProfileBtn.tsx
+++ src/components/layout/Navbar/TopNavbar/ProfileBtn.tsx
@@ -1,6 +1,7 @@
-const ProfileBtn = () => {
-    const { user } = useAppSelector((state) => state.auth); // Get user state
+import Link from "next/link";
+import Image from "next/image";
 
+const ProfileBtn = () => {
     return (
         <Link href="/profile" className="p-1 flex items-center">
             <Image
@@ -11,11 +12,31 @@ const ProfileBtn = () => {
                 alt="profile"
                 className="max-w-[22px] max-h-[22px]"
             />
-            {user?.name && (
-                <span className="ml-2 text-sm font-medium hidden md:inline">
-                    {user.name.split(" ")[0]} {/* Show only first name */}
-                </span>
-            )}
         </Link>
     );
 };
+
+export default ProfileBtn;
+
+// In case u need authentication
+// const { user } = useAppSelector((state) => state.auth); Get user state
+// const profilebtn = () => {
+
+//     return (
+//         <Link href="/profile" className="p-1 flex items-center">
+//             <Image
+//                 priority
+//                 src="/icons/user.svg"
+//                 height={22}
+//                 width={22}
+//                 alt="profile"
+//                 className="max-w-[22px] max-h-[22px]"
+//             />
+//             {user?.name && (
+//                 <span className="ml-2 text-sm font-medium hidden md:inline">
+//                     {user.name.split(" ")[0]} {/* Show only first name */}
+//                 </span>
+//             )}
+//         </Link>
+//     );
+// };
